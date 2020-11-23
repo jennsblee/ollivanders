44356c097be9f1d2af8d56becf4e8f03adfe068a
Victoria Draper <victoria.draper27@gmail.com>
2020-11-23 14:06:12 +0900

    ARCH: Added Edit View & Created Edit, Update, Destory Controller Action Methods

--- app/controllers/wands_controller.rb
+++ app/controllers/wands_controller.rb
@@ -1,4 +1,6 @@
 class WandsController < ApplicationController
+  before_action :set_wand, only: [:show, :edit, :update, :destroy]
+
   def home
   end
 
@@ -14,12 +16,36 @@ class WandsController < ApplicationController
   def show
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @wand = Wand.new(wand_params)
+    @wand.user = current_user
+
+    if @wand.save
+      redirect_to wand_path(@wand), notice: 'Wand was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @wand = Wand.find(params[:id])
+    @user = @wand.user
+    @wand.destroy
+
+    #eventually we would want to redirect to the users(wands page) where they can see all their wands,
+    #since they are the one seeing this
+    redirect_to wands_path
+  end
+
+  private
+
+  def set_wand
+    @wand = Wand.find(params[:id])
+  end
+
+  def wand_params
+    params.require(:wand).permit(:name, :wood, :core, :price, :size, :description)
   end
 end
