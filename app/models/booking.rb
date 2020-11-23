653acb8047e4ec7ed867934cf56ed15023d6cda7
Jennifer Lee <jlee970124@gmail.com>
2020-11-23 15:09:29 +0900

    ARCH: added validations and relations in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :wand
+
+  validates :check_out, :check_in, presence: true
 end
--- app/models/wand.rb
+++ app/models/wand.rb
@@ -1,3 +1,8 @@
 class Wand < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :wood, presence: true, uniqueness: { scope: :core }
+  validates :price, presence: true
+  validates :size, presence: true
 end
