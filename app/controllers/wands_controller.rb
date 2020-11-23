63d9c76197ff2cfbf1fa3bdb77e324ea52481180
Jennifer Lee <jlee970124@gmail.com>
2020-11-23 12:51:10 +0900

    ARCH: created actions and routes for user stories

--- app/controllers/wands_controller.rb
+++ app/controllers/wands_controller.rb
@@ -1,4 +1,25 @@
 class WandsController < ApplicationController
   def home
   end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'bookings/create'
   devise_for :users
   root to: 'wands#home'
+
+  resources :wands do
+    resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
