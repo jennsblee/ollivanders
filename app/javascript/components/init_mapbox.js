22df7d70ee6aec2b6c9364b29577148b4c6a60ef
Victoria Draper <victoria.draper27@gmail.com>
2020-11-24 23:28:46 +0900

    FEAT: Added a MapBox Custom Map Harry Potter Vibes

--- app/javascript/components/init_mapbox.js
+++ app/javascript/components/init_mapbox.js
@@ -1,43 +1,51 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-};
-
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 90, maxZoom: 15, duration: 3000 });
+  };
 
   const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // add this
-      .addTo(map);
-  });
-};
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-
-    const markers = JSON.parse(mapElement.dataset.markers);
 
     markers.forEach((marker) => {
-    new mapboxgl.Marker()
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '40px';
+    element.style.height = '40px';
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
   });
   }
 
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    // Generate map on page
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/ollivanders/ckhvz8v5j0v8r19o6xlmtbqod'
+    });
 
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    // Add markers to page
+    addMapToMarkers(map, markers);
+    fitMapToMarkers(map, markers);
+
+    // Adding fancy little search bar
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }));
+  }
 };
 
 export { initMapbox };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,12 +31,14 @@ import { initSelect2 } from '../components/init_select2';
 import { initSweetalert } from '../components/init_sweetalert';
 import '../components/init_flatpickr';
 import { initMapbox } from '../components/init_mapbox';
+import { initAutocomplete } from "../components/init_autocomplete";
 
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
   initMapbox();
+  initAutocomplete();
   initSweetalert('#sweet-alert-demo', {
       title: "A nice alert",
       text: "This is a great alert, isn't it?",
