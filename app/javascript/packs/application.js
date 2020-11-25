28c5904f56c5c47f10f82e3ce645f4abe362b738
Rachael Savoy <rachael.a.hakim@gmail.com>
2020-11-25 12:34:17 +0900

    FIX: carousel is working.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,7 +37,8 @@ import { initAutocomplete } from "../components/init_autocomplete";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSelect2();
-  initMapbox();
+  // initMapbox();
+  $('.carousel').carousel()
   initAutocomplete();
   initSweetalert('#sweet-alert-demo', {
       title: "A nice alert",
