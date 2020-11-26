bfcf539792ab35afa1b01021fefd5b49f174915c
Victoria Draper <victoria.draper27@gmail.com>
2020-11-26 16:25:17 +0900

    FEAT: Updated Bookings Policy Controller

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -8,4 +8,18 @@ class BookingPolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def update?
+    renter_or_admin_or_owner?
+  end
+
+  def destroy?
+    renter_or_admin_or_owner?
+  end
+
+  private
+
+  def renter_or_admin_or_owner?
+    user == record.user || user.admin || record.wand.user
+  end
 end
