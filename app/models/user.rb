06a9da6bbdda158f01e4576da2dd7e3431f7ee34
Rachael Savoy <rachael.a.hakim@gmail.com>
2020-11-27 13:15:02 +0900

    FEAT: added requests tab lists pending requests, and added approve or reject buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :bookings
   has_many :wands, dependent: :destroy
+  has_many :received_bookings, class_name: 'Booking', through: :wands, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
